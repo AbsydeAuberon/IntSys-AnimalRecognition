8ed6fdb6dd5427d111098e6ee1cf96b81f2e66b1
Álvaro Tomás Lozano <auberontermeraz@gmail.com>
2020-01-20 12:17:50 +0100

    Script for changing names

---
+++ ChangeFileName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ChangeFileName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(@"C:\Users\auber\Desktop\MACHINELEARNING\Rooms");
+                int i = 1;
+
+                foreach (var file in d.GetFiles())
+                {
+                    Directory.Move(file.FullName, @"C:\Users\auber\Desktop\MACHINELEARNING\Rooms\" + "Room_" + i.ToString() + ".jpg");
+                    i++;
+                }
+
+                Console.WriteLine("Satisfactory rename of the files.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
